48a29a43b3eb8d27262eed095910285c0f757e61
Alexander Orobinskiy <alexander.orobinskiy@techs.com.ua>
2019-07-22 17:24:21 +0300

    Refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,7 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   Future<ArticlesResponse> post;
   String apiKeyForPost;
+
   @override
   void initState() {
     post = fetchPost();
@@ -43,7 +44,8 @@ class _MyHomePageState extends State<MyHomePage> {
 
   Future<ArticlesResponse> fetchPost() async {
     var secret = await getSecret();
-    final response = await get('https://api.nytimes.com/svc/archive/v1/2019/1.json?api-key=${secret.apiKey}');
+    final response = await get(
+        'https://api.nytimes.com/svc/archive/v1/2019/1.json?api-key=${secret.apiKey}');
 
     return ArticlesResponse.fromJson(json.decode(response.body));
   }
@@ -51,84 +53,101 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: new Image.asset('images/nyt.png', fit: BoxFit.cover),
-      ),
-      body: FutureBuilder<ArticlesResponse>(
-              future: post,
-              builder: (context, snapshot) {
-                if (snapshot.hasData) {
-                  return ListView.builder(
-                    itemBuilder: (context, position) {
-                      return Column(
+        appBar: AppBar(
+          title: new Image.asset('images/nyt.png', fit: BoxFit.cover),
+        ),
+        body: FutureBuilder<ArticlesResponse>(
+          future: post,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return ListView.builder(
+                itemBuilder: (context, position) {
+                  return Column(
+                    children: <Widget>[
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: <Widget>[
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          Expanded(
+                              child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
                             children: <Widget>[
-                              Expanded(child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: <Widget>[
-                                  Padding(
-                                    padding:
-                                    const EdgeInsets.fromLTRB(12.0, 12.0, 12.0, 6.0),
-                                    child: Text(
-                                      snapshot.data.response.articles[position].snippet,
-                                      style: TextStyle(
-                                          fontSize: 22.0, fontWeight: FontWeight.bold),
-                                      overflow: TextOverflow.fade,
-                                    ),
-                                  ),
-                                  Padding(
-                                    padding:
-                                    const EdgeInsets.fromLTRB(12.0, 6.0, 12.0, 12.0),
-                                    child: Text(
-                                      DateTime.parse(snapshot.data.response.articles[position].pubDate).toLocal().toString().substring(0, snapshot.data.response.articles[position].pubDate.length-5),
-                                      style: TextStyle(fontSize: 18.0),
-                                    ),
-                                  ),
-                                ],
-                              )),
                               Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                                  children: <Widget>[
-                                    Text(
-                                      "5m",
-                                      style: TextStyle(color: Colors.grey),
-                                    ),
-                                    Padding(
-                                      padding: const EdgeInsets.all(8.0),
-                                      child: Icon(
-                                        Icons.star_border,
-                                        size: 35.0,
-                                        color: Colors.grey,
-                                      ),
-                                    ),
-                                  ],
+                                padding: const EdgeInsets.fromLTRB(
+                                    12.0, 12.0, 12.0, 6.0),
+                                child: Text(
+                                  snapshot
+                                      .data.response.articles[position].snippet,
+                                  style: TextStyle(
+                                      fontSize: 22.0,
+                                      fontWeight: FontWeight.bold),
+                                  overflow: TextOverflow.fade,
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.fromLTRB(
+                                    12.0, 6.0, 12.0, 12.0),
+                                child: Text(
+                                  DateTime.parse(snapshot.data.response
+                                          .articles[position].pubDate)
+                                      .toLocal()
+                                      .toString()
+                                      .substring(
+                                          0,
+                                          snapshot
+                                                  .data
+                                                  .response
+                                                  .articles[position]
+                                                  .pubDate
+                                                  .length -
+                                              5),
+                                  style: TextStyle(fontSize: 18.0),
                                 ),
                               ),
                             ],
+                          )),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: <Widget>[
+                                Text(
+                                  "5m",
+                                  style: TextStyle(color: Colors.grey),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Icon(
+                                    Icons.star_border,
+                                    size: 35.0,
+                                    color: Colors.grey,
+                                  ),
+                                ),
+                              ],
+                            ),
                           ),
-                          Divider(
-                            height: 2.0,
-                            color: Colors.grey,
-                          )
                         ],
-                      );
-                    },
-                    itemCount: snapshot.data.response.articles.length,
+                      ),
+                      Divider(
+                        height: 2.0,
+                        color: Colors.grey,
+                      )
+                    ],
                   );
-                } else if (snapshot.hasError) {
-                  return Text("${snapshot.error}");
-                }
-                // By default, show a loading spinner.
-                return Center(child: CircularProgressIndicator(),);
-              },
-            )
-    );
+                },
+                itemCount: snapshot.data.response.articles.length,
+              );
+            } else if (snapshot.hasError) {
+              return Text("${snapshot.error}");
+            }
+            // By default, show a loading spinner.
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          },
+        ));
   }
 }
+
 class ArticlesResponse {
   final String copyright;
   final NYTResponse response;
@@ -137,10 +156,6 @@ class ArticlesResponse {
 
   factory ArticlesResponse.fromJson(Map<String, dynamic> json) {
     var res = NYTResponse.fromJson(json['response']);
-    return ArticlesResponse(
-      copyright: json['copyright'],
-      response: res
-    );
+    return ArticlesResponse(copyright: json['copyright'], response: res);
   }
 }
-
